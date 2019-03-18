b9863ba356ce8abbd45674052586d74de447a96e
Matthew Frankland <mfrankland@hotmail.co.uk>
2019-03-18 10:59:11 +0000

    final copy

--- F29SO/Pods/Target Support Files/Pods-F29SOTests/Pods-F29SOTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_F29SOTests : NSObject
-@end
-@implementation PodsDummy_Pods_F29SOTests
-@end
--- F29SO/Pods/Target Support Files/Pods-F29SOUITests/Pods-F29SOUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_F29SOUITests : NSObject
-@end
-@implementation PodsDummy_Pods_F29SOUITests
-@end
